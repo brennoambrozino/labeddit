af1228a55bc7eef1cefbab5980bdf976339266a3
brennoambrozino <brennoambrozino@gmail.com>
2022-07-14 19:39:12 -0300

    labeddit

---
+++ labeddit/src/components/Loading/Styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LoadingContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 30px;
+`
